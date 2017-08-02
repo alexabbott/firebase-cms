96e68bea396840e1c7fcb0c6490c8e469766014f
Alex Abbott <alex.abbott@bynd.com>
2017-08-02 18:11:26 -0400

    Remove console log

--- src/app/pipes/get.pipe.ts
+++ src/app/pipes/get.pipe.ts
@@ -5,8 +5,6 @@ export class GetPipe implements PipeTransform {
   transform(val, args) {
     if (val === null || !val || !val[0]) return val;
     if (val) {
-      console.log('val', val);
-      console.log('args', args);
       return val.filter((v) => {
         return v.uid === args;
       })[0];
