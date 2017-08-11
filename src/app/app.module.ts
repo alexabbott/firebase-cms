169827c943eea58118670376950a52c9de7fff47
Andrew Kemp <andrew.kemp@bynd.com>
2017-08-11 13:03:02 -0400

    Converting app.module to use app/ paths instead of ./

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
+import { AppComponent } from 'app/app.component';
 import { FormsModule } from '@angular/forms';
 import { environment } from '../environments/environment';
 import { AngularFireModule } from 'angularfire2';
@@ -33,55 +33,55 @@ import { DndModule } from 'ng2-dnd';
 import { AppRoutingModule } from 'app/app-routing.module';
 
 // components
-import { PostsComponent } from './storefront-components/posts/posts.component';
-import { AdminComponent } from './admin-components/admin/admin.component';
-import { LoginComponent } from './admin-components/login/login.component';
-import { AddPostComponent } from './admin-components/add-post/add-post.component';
-import { AdminPostsComponent } from './admin-components/admin-posts/admin-posts.component';
-import { AdminAdminsComponent } from './admin-components/admin-admins/admin-admins.component';
-import { AdminDashboardComponent } from './admin-components/admin-dashboard/admin-dashboard.component';
-import { AddAdminComponent } from './admin-components/add-admin/add-admin.component';
-import { PagesComponent } from './storefront-components/pages/pages.component';
-import { AdminPagesComponent } from './admin-components/admin-pages/admin-pages.component';
-import { PageComponent } from './storefront-components/page/page.component';
-import { AddPageComponent } from './admin-components/add-page/add-page.component';
-import { PostComponent } from './storefront-components/post/post.component';
-import { AdminMenusComponent } from './admin-components/admin-menus/admin-menus.component';
-import { AdminThemeComponent } from './admin-components/admin-theme/admin-theme.component';
-import { ProductsComponent } from './storefront-components/products/products.component';
-import { ProductComponent } from './storefront-components/product/product.component';
-import { AdminProductsComponent } from './admin-components/admin-products/admin-products.component';
-import { AddProductComponent } from './admin-components/add-product/add-product.component';
-import { CartComponent } from './storefront-components/cart/cart.component';
-import { DeleteDialogComponent } from './admin-components/delete-dialog/delete-dialog.component';
-import { CartIconComponent } from './storefront-components/cart-icon/cart-icon.component';
-import { AdminCustomersComponent } from './admin-components/admin-customers/admin-customers.component';
-import { CheckoutShippingComponent } from './storefront-components/checkout-shipping/checkout-shipping.component';
-import { CheckoutBillingComponent } from './storefront-components/checkout-billing/checkout-billing.component';
-import { CheckoutPaymentComponent } from './storefront-components/checkout-payment/checkout-payment.component';
-import { CheckoutReviewComponent } from './storefront-components/checkout-review/checkout-review.component';
-import { CheckoutConfirmationComponent } from './storefront-components/checkout-confirmation/checkout-confirmation.component';
-import { AddOrderComponent } from './admin-components/add-order/add-order.component';
-import { AddCustomerComponent } from './admin-components/add-customer/add-customer.component';
-import { OrdersComponent } from './storefront-components/orders/orders.component';
-import { OrderComponent } from './storefront-components/order/order.component';
-import { AdminOrdersComponent } from './admin-components/admin-orders/admin-orders.component';
-import { AdminCustomerComponent } from './admin-components/admin-customer/admin-customer.component';
+import { PostsComponent } from 'app/storefront-components/posts/posts.component';
+import { AdminComponent } from 'app/admin-components/admin/admin.component';
+import { LoginComponent } from 'app/admin-components/login/login.component';
+import { AddPostComponent } from 'app/admin-components/add-post/add-post.component';
+import { AdminPostsComponent } from 'app/admin-components/admin-posts/admin-posts.component';
+import { AdminAdminsComponent } from 'app/admin-components/admin-admins/admin-admins.component';
+import { AdminDashboardComponent } from 'app/admin-components/admin-dashboard/admin-dashboard.component';
+import { AddAdminComponent } from 'app/admin-components/add-admin/add-admin.component';
+import { PagesComponent } from 'app/storefront-components/pages/pages.component';
+import { AdminPagesComponent } from 'app/admin-components/admin-pages/admin-pages.component';
+import { PageComponent } from 'app/storefront-components/page/page.component';
+import { AddPageComponent } from 'app/admin-components/add-page/add-page.component';
+import { PostComponent } from 'app/storefront-components/post/post.component';
+import { AdminMenusComponent } from 'app/admin-components/admin-menus/admin-menus.component';
+import { AdminThemeComponent } from 'app/admin-components/admin-theme/admin-theme.component';
+import { ProductsComponent } from 'app/storefront-components/products/products.component';
+import { ProductComponent } from 'app/storefront-components/product/product.component';
+import { AdminProductsComponent } from 'app/admin-components/admin-products/admin-products.component';
+import { AddProductComponent } from 'app/admin-components/add-product/add-product.component';
+import { CartComponent } from 'app/storefront-components/cart/cart.component';
+import { DeleteDialogComponent } from 'app/admin-components/delete-dialog/delete-dialog.component';
+import { CartIconComponent } from 'app/storefront-components/cart-icon/cart-icon.component';
+import { AdminCustomersComponent } from 'app/admin-components/admin-customers/admin-customers.component';
+import { CheckoutShippingComponent } from 'app/storefront-components/checkout-shipping/checkout-shipping.component';
+import { CheckoutBillingComponent } from 'app/storefront-components/checkout-billing/checkout-billing.component';
+import { CheckoutPaymentComponent } from 'app/storefront-components/checkout-payment/checkout-payment.component';
+import { CheckoutReviewComponent } from 'app/storefront-components/checkout-review/checkout-review.component';
+import { CheckoutConfirmationComponent } from 'app/storefront-components/checkout-confirmation/checkout-confirmation.component';
+import { AddOrderComponent } from 'app/admin-components/add-order/add-order.component';
+import { AddCustomerComponent } from 'app/admin-components/add-customer/add-customer.component';
+import { OrdersComponent } from 'app/storefront-components/orders/orders.component';
+import { OrderComponent } from 'app/storefront-components/order/order.component';
+import { AdminOrdersComponent } from 'app/admin-components/admin-orders/admin-orders.component';
+import { AdminCustomerComponent } from 'app/admin-components/admin-customer/admin-customer.component';
 
 // services
-import { GlobalService } from './services/global.service';
-import { WindowRefService } from './services/window-ref.service';
-import { LocalCartService } from './services/localcart.service';
+import { GlobalService } from 'app/services/global.service';
+import { WindowRefService } from 'app/services/window-ref.service';
+import { LocalCartService } from 'app/services/localcart.service';
 
 // guards
-import { AuthGuard } from './services/auth.guard';
+import { AuthGuard } from 'app/services/auth.guard';
 
 // pipes
-import { SortPipe } from './pipes/sort.pipe';
-import { SafeHtmlPipe } from './pipes/safe-html.pipe';
-import { TruncatePipe } from './pipes/truncate.pipe';
-import { GetPipe } from './pipes/get.pipe';
-import { ObjectCountPipe } from './pipes/object-count.pipe';
+import { SortPipe } from 'app/pipes/sort.pipe';
+import { SafeHtmlPipe } from 'app/pipes/safe-html.pipe';
+import { TruncatePipe } from 'app/pipes/truncate.pipe';
+import { GetPipe } from 'app/pipes/get.pipe';
+import { ObjectCountPipe } from 'app/pipes/object-count.pipe';
 
 firebase.initializeApp(environment.firebase);
 
